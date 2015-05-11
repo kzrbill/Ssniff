d5e6a82c8f086d1fd13ac4265b5b498e30594ac8
will.grange <will.grange@zeninternet.co.uk>
2015-05-11 22:05:24 +0100

    Inititial sniff for smells test. Node modules added.

--- test/solutionSniffer.spec.js
+++ test/solutionSniffer.spec.js
@@ -1,5 +1,35 @@
+var module = require('../lib/solutionSniffer.js');
+var SolutionSniffer = module.SolutionSniffer;
+
 describe('Solution sniffer', function() {
-	it('should not behave...', function() {
-		expect(false).toBe(true);
+
+	var sniffer;
+
+	beforeEach(function(){
+		sniffer = new SolutionSniffer();
 	});
+
+	it("can create instances", function() {
+
+		expect(sniffer).toBeDefined();
+	});
+
+
+	it("can run sniffs", function() {
+
+		var ranSniff = false;
+
+		function SomeSmell()
+		{
+			this.sniff = function(){
+				ranSniff = true;
+			}
+		}
+
+		sniffer.addSmell(new SomeSmell());
+
+		sniffer.sniffForSmells();
+
+    expect(ranSniff).toBe(true);
+  });
 });
