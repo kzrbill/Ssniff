26cb68e4379d326ed73e931ea37684fe2ba4dcad
will.grange <will.grange@zeninternet.co.uk>
2015-05-17 10:19:09 +0100

    Cleaned up sniff - extracted output to files.

--- sniff.js
+++ sniff.js
@@ -1,55 +1,10 @@
 
 var SolutionSniffer = require(__dirname + '/lib/solutionSniffer.js').SolutionSniffer;
 var Solution = require(__dirname + '/lib/solution.js').Solution;
+var SniffOutput = require(__dirname + '/lib/sniffOutput.js').SniffOutput;
+var ViewObject = require(__dirname + '/lib/viewObject.js').ViewObject;
 
-
-var notifier = require('node-notifier');
-var eyes = require('eyes');
-
-var errorInspector = eyes.inspector({
-    styles: {
-        all: 'red'
-    }
-});
-
-var okInspector = eyes.inspector({
-    styles: {
-        all: 'green'
-    }
-});
-
-var warningInspector = eyes.inspector({
-    styles: {
-        all: 'cyan'
-    }
-});
-
-
-function SniffResultsOutput() {
-	this.onSmellResultFound = function(smellResult) {
-
-		var viewObject = smellResult.viewObject();
-
-		errorInspector(viewObject);
-
-		notifier.notify({
-		  'title': viewObject.description,
-		  'message': viewObject.message
-		});
-		 
-	}
-
-	this.onOKResultFound = function(smellResult) {
-		okInspector(smellResult.viewObject()); 
-	}
-
-	this.onWarningResultFound = function(smellResult) {
-		warningInspector(smellResult.viewObject()); 
-	}
-}
-
-var solutionPath = __dirname + "/test/test_solutions/stinked_solution/"
-
+var solutionPath = __dirname + "/test/test_solutions/stinked_solution/";
 var args = process.argv.slice(2);
 
 if (args.length >= 1) {
@@ -58,20 +13,22 @@ if (args.length >= 1) {
 	console.log("No path supplied so using test solution path: " + solutionPath);
 }
 
-function SolutionSnifferStarter()
+var ouput = new SniffOutput();
+
+function SniffRunner()
 {
-	this.onSolutionFound = function(validationSolution)
+	this.onSolutionFound = function(solution)
 	{
-		var solution = new Solution(validationSolution.dirPath);
+		solution = solution.copy(); // ~ use copy to prevent dependency recurson.
 		var solutionSniffer = new SolutionSniffer(solution);
-		solutionSniffer.sniffForSmells(new SniffResultsOutput());
+		solutionSniffer.sniffForSmells(ouput);
 	}
 
-	this.onSolutionNotFound = function(validationSolution)
+	this.onSolutionNotFound = function(solution)
 	{
-		errorInspector("No solution found at path: " + validationSolution.dirPath);
+		var viewObject = new ViewObject("Solution not found", "No solution found at path: " + solution.dirPath);
+		ouput.outputError(viewObject);
 	}
 }
 
-new Solution(solutionPath).checkSolutionExists(new SolutionSnifferStarter());
-
+new Solution(solutionPath).checkSolutionExists(new SniffRunner());
