c923f60be14f6c24aeef86d9fbed539d1dd96da6
unknown <will.grange@9PS0P02.office.zen.co.uk>
2015-05-19 13:42:32 +0100

    Difference machine beginnings, README fixes and jasmine_node added

--- test/differenceMachine.spec.js
+++ test/differenceMachine.spec.js
@@ -1,16 +1,39 @@
 
-function DifferenceMachine(diffKey)
+function DifferenceMachine(valueKey, idKey)
 {
-	this.diffKey = diffKey;
+	this.valueKey = valueKey;
+	this.idKey = idKey;
+	this.diffGroups = [];
 
 	this.add = function(object)
 	{
-		return this;
+		var theValue = object[this.valueKey];
+
+		if (this.diffGroups[theValue] == undefined)
+		{
+			this.diffGroups[theValue] = new DifferenceGroup(theValue);
+		} else {
+			this.diffGroups[theValue].add(this.idKey);
+		}
 	}
 
 	this.differences = function()
 	{
-		return [];
+		if (this.diffGroups.length < 2)
+			return [];
+
+		return this.diffGroups;
+	}
+}
+
+function DifferenceGroup(value)
+{
+	this.names = [];
+	this.value = value;
+
+	this.add = function(name)
+	{
+		this.names.push(name);
 	}
 }
 
@@ -34,6 +57,12 @@ describe('DifferenceMachine', function() {
 		SomeSpecialProperty : 2
 	}
 
+	var someObjectD =
+	{
+		Name : "SomeObjectD",
+		SomeSpecialProperty : 3
+	}
+
 	var diffMachine = null;
 	beforeEach(function() {
 		
@@ -43,10 +72,12 @@ describe('DifferenceMachine', function() {
 	
 	describe('when matching objects supplied', function() {
 
-		it('should have no differences', function() {
+		xit('should have no differences', function() {
 
-			diffMachine = diffMachine.add(someObjectA)
-									 .add(someObjectB);
+			diffMachine.add(someObjectA);
+			diffMachine.add(someObjectB);
+
+			console.log("Length: " + diffMachine.differences().length);
 
 			expect(diffMachine.differences().length).toEqual(0);
 		});
@@ -56,11 +87,40 @@ describe('DifferenceMachine', function() {
 
 		xit('should have difference', function() {
 
-			diffMachine = diffMachine.add(someObjectA)
-									 .add(someObjectB)
-									 .add(someObjectC);
+			diffMachine.add(someObjectA);
+			diffMachine.add(someObjectB);
+			diffMachine.add(someObjectC);
 
-			expect(diffMachine.differences().length).toEqual(1);
+			var diffGroup1 = new DifferenceGroup(1);
+			diffGroup1.add("SomeObjectA");
+			diffGroup1.add("SomeObjectB");
+
+			var diffGroup2 = new DifferenceGroup(2);
+			diffGroup2.add("SomeObjectC");
+
+			var differences =
+			[
+				diffGroup1,
+				diffGroup2
+			];
+
+			// SomeObjectA and SomeObjectB have value 1
+			// SomeObjectC has value 2
+			expect(diffMachine.differences()).toEqual(differences);
 		});
+
+		xit('should identify someObjectC as being different', function() {
+
+			diffMachine.add(someObjectA);
+			diffMachine.add(someObjectB);
+			diffMachine.add(someObjectC);
+			diffMachine.add(someObjectD);
+
+			// SomeObjectA and SomeObjectB have value 1
+			// SomeObjectC has value 2 
+			// someObjectD has value 3
+			expect(diffMachine.differences()[0]).toEqual(someObjectC);
+		});
+
 	});
 });
